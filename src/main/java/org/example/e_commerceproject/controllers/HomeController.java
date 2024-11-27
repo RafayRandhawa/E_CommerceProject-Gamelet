334268d5ced2c85a5183959f01b9f89d326ed57b
DESKTOP-DIOV3FA\hp Zbook <mrafay280@gmail.com>
2024-11-27 16:19:17 +0500

    Home Page Started

---
+++ src/main/java/org/example/e_commerceproject/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package org.example.e_commerceproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+}
+
