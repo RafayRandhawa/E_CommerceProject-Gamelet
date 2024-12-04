4bae849139230ae1f4622c617a58b398ac69fc44
DESKTOP-DIOV3FA\hp Zbook <mrafay280@gmail.com>
2024-12-05 04:18:35 +0500

    Admin Panel Fixed

--- src/main/java/org/example/e_commerceproject/controllers/AdminController.java
+++ src/main/java/org/example/e_commerceproject/controllers/AdminController.java
@@ -96,8 +96,7 @@ public class AdminController {
 
     @DeleteMapping("/products/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        System.out.println("---------------------------------------------------------------");
-        System.out.println("product to delete has id:  "+id);
+
         adminService.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
